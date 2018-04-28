62bc6db6fcf151518bd305a1bc29ee2e974f9864
Leomar Camargo <leomarcamargodesouza@gmail.com>
2018-04-28 20:37:43 -0300

    Adding 'staticCategory' preprocessor directive on AddReminderActivity class

--- reminder-cc/src/br/unb/cic/reminders/view/AddReminderActivity.java
+++ reminder-cc/src/br/unb/cic/reminders/view/AddReminderActivity.java
@@ -3,6 +3,10 @@ package br.unb.cic.reminders.view;
 import br.unb.cic.framework.persistence.DBException;
 import br.unb.cic.reminders.controller.Controller;
 import br.unb.cic.reminders.model.Reminder;
+//#ifdef staticCategory 
+import br.unb.cic.reminders.model.Category;
+import java.util.List;
+//#endif 
 
 public class AddReminderActivity extends ReminderActivity {
 	@Override
@@ -11,10 +15,30 @@ public class AddReminderActivity extends ReminderActivity {
 
 	@Override
 	protected void persist(Reminder reminder) {
+		//#ifdef staticCategory
+		try {
+			Category category = findCategory(reminder.getCategory());
+			reminder.setCategory(category);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//#endif
+
 		try {
 			Controller.instance(getApplicationContext()).addReminder(reminder);
 		} catch (DBException e) {
 			e.printStackTrace();
 		}
 	}
+
+	//#ifdef staticCategory
+	private Category findCategory(Category category) throws Exception {
+		List<Category> categories = Controller.instance(getApplicationContext()).listCategories();
+		for (Category c : categories) {
+			if (c.getName().equals(category.getName()))
+				return c;
+		}
+		return null;
+	}
+	//#endif
 }
