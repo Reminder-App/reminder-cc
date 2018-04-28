f05b8efed014589f740a390aaceb8bde4ee9a043
Leomar Camargo <leomarcamargodesouza@gmail.com>
2018-04-28 20:37:24 -0300

    Adding 'staticCategory' preprocessor directive on ReminderDAO class

--- reminder-cc/src/br/unb/cic/reminders/model/db/ReminderDAO.java
+++ reminder-cc/src/br/unb/cic/reminders/model/db/ReminderDAO.java
@@ -3,6 +3,9 @@ package br.unb.cic.reminders.model.db;
 import java.util.List;
 import br.unb.cic.framework.persistence.DBException;
 import br.unb.cic.reminders.model.Reminder;
+//#ifdef staticCategory 
+import br.unb.cic.reminders.model.Category;
+//#endif 
 
 public interface ReminderDAO {
 	public Long saveReminder(Reminder r) throws DBException;
@@ -14,4 +17,8 @@ public interface ReminderDAO {
 	public void deleteReminder(Reminder reminder) throws DBException;
 
 	public void persistReminder(Reminder reminder) throws DBException;
+
+	//#ifdef staticCategory
+	public List<Reminder> listRemindersByCategory(Category category) throws DBException;
+	//#endif
 }
