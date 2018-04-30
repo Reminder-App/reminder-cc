4f878d833a96140ed301a89745cda99dfef32a2a
Leomar Camargo <leomarcamargodesouza@gmail.com>
2018-04-30 00:09:48 -0300

    Adding 'manageCategory' preprocessor directive on AddReminderActivity class

--- reminder-cc/src/br/unb/cic/reminders/view/AddReminderActivity.java
+++ reminder-cc/src/br/unb/cic/reminders/view/AddReminderActivity.java
@@ -3,7 +3,7 @@ package br.unb.cic.reminders.view;
 import br.unb.cic.framework.persistence.DBException;
 import br.unb.cic.reminders.controller.Controller;
 import br.unb.cic.reminders.model.Reminder;
-//#ifdef staticCategory 
+//#if staticCategory || manageCategory 
 import br.unb.cic.reminders.model.Category;
 import java.util.List;
 //#endif 
@@ -15,10 +15,19 @@ public class AddReminderActivity extends ReminderActivity {
 
 	@Override
 	protected void persist(Reminder reminder) {
-		//#ifdef staticCategory
+		//#if staticCategory || manageCategory
 		try {
 			Category category = findCategory(reminder.getCategory());
+			//#ifdef manageCategory
+			if (category != null) {
+				reminder.setCategory(category);
+			} else {
+				Controller.instance(getApplicationContext()).addCategory(reminder.getCategory());
+				reminder.setCategory(findCategory(reminder.getCategory()));
+			}
+			//#elifdef staticCategory
 			reminder.setCategory(category);
+			//#endif
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -31,7 +40,7 @@ public class AddReminderActivity extends ReminderActivity {
 		}
 	}
 
-	//#ifdef staticCategory
+	//#if staticCategory || manageCategory
 	private Category findCategory(Category category) throws Exception {
 		List<Category> categories = Controller.instance(getApplicationContext()).listCategories();
 		for (Category c : categories) {
