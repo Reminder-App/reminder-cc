0990fe25769105d663e0b679bf35e24828328a81
Leomar Camargo <leomarcamargodesouza@gmail.com>
2018-05-20 00:06:13 -0300

    Adding the preprocessor directives 'fixedDate' and 'dateRange' on Reminder class

--- reminder-cc/src/br/unb/cic/reminders/model/Reminder.java
+++ reminder-cc/src/br/unb/cic/reminders/model/Reminder.java
@@ -19,10 +19,22 @@ public class Reminder {
 	private String text;
 	@Column(column = "DETAILS", type = DBTypes.TEXT)
 	private String details;
+	//#ifdef fixedDate
 	@Column(column = "DATE", type = DBTypes.TEXT)
 	private String date;
 	@Column(column = "HOUR", type = DBTypes.TEXT)
 	private String hour;
+	//#endif
+	//#ifdef dateRange
+	@Column(column = "INITIAL_DATE", type = DBTypes.TEXT)
+	private String dateStart;
+	@Column(column = "INITIAL_HOUR", type = DBTypes.TEXT)
+	private String hourStart;
+	@Column(column = "FINAL_DATE", type = DBTypes.TEXT)
+	private String dateFinal;
+	@Column(column = "FINAL_HOUR", type = DBTypes.TEXT)
+	private String hourFinal;
+	//#endif
 	@Column(column = "DONE", type = DBTypes.INT)
 	private boolean done;
 
@@ -76,6 +88,7 @@ public class Reminder {
 		}
 	}
 
+	//#ifdef fixedDate
 	public String getDate() {
 		return date;
 	}
@@ -97,6 +110,59 @@ public class Reminder {
 		}
 		this.hour = hour;
 	}
+	//#endif
+
+	//#ifdef dateRange
+	public String getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(String dateStart) {
+
+		if (dateStart == null)
+			throw new InvalidDateException(dateStart);
+		if (!(dateStart == null || dateStart.equals("")) && !checkFormat(dateStart, Patterns.DATE_PATTERN)) {
+			throw new InvalidDateException(dateStart);
+		}
+		this.dateStart = dateStart;
+	}
+
+	public String getHourStart() {
+		return hourStart;
+	}
+
+	public void setHourStart(String hourStart) {
+		if (!(hourStart == null || hourStart.equals("")) && !checkFormat(hourStart, Patterns.HOUR_PATTERN)) {
+			throw new InvalidHourException(hourStart);
+		}
+		this.hourStart = hourStart;
+	}
+
+	public String getDateFinal() {
+		return dateFinal;
+	}
+
+	public void setDateFinal(String dateFinal) {
+
+		if (dateFinal == null)
+			throw new InvalidDateException(dateFinal);
+		if (!(dateFinal == null || dateFinal.equals("")) && !checkFormat(dateFinal, Patterns.DATE_PATTERN)) {
+			throw new InvalidDateException(dateFinal);
+		}
+		this.dateFinal = dateFinal;
+	}
+
+	public String getHourFinal() {
+		return hourFinal;
+	}
+
+	public void setHourFinal(String hourFinal) {
+		if (!(hourFinal == null || hourFinal.equals("")) && !checkFormat(hourFinal, Patterns.HOUR_PATTERN)) {
+			throw new InvalidHourException(hourFinal);
+		}
+		this.hourFinal = hourFinal;
+	}
+	//#endif
 
 	private boolean checkFormat(String date, String pattern) {
 		Pattern p = Pattern.compile(pattern);
@@ -125,13 +191,19 @@ public class Reminder {
 	//#endif
 
 	public boolean isValid() {
-		return (text != null && date != null && hour != null
-		//#if staticCategory || manageCategory
+		return (text != null
+				//#ifdef fixedDate
+				&& date != null && hour != null
+				//#endif
+				//#ifdef dateRange
+				&& dateStart != null && hourStart != null && dateFinal != null && hourFinal != null
+				//#endif
+				//#if staticCategory || manageCategory
 				&& category != null
-		//#endif
-		//#ifdef priority 
-				&& priority != null 
-	    //#endif 
+				//#endif
+				//#ifdef priority
+				&& priority != null
+				//#endif
 		);
 	}
 
